9d8ea578a7c3f8ec885364c8682ee274d368c3de
dbc2201 <divyanshb22@gmail.com>
2022-06-20 00:31:28 +0530

    update test cases

--- src/test/java/io/github/dbc/SeriesSumFinderTest.java
+++ src/test/java/io/github/dbc/SeriesSumFinderTest.java
@@ -16,30 +16,19 @@ class SeriesSumFinderTest {
 
     @ParameterizedTest(name = "calculateSumOfSeries({arguments})")
     @CsvSource(value = {
-            "1,1.0",
-            "2,1.5",
-            "4,2.0833",
-            "6,2.4499",
-            "8,2.7178",
-            "10,2.9289",
-            "12,3.1032",
-            "14,3.2515",
-            "16,3.3807",
-            "18,3.4951",
-            "20,3.5977",
-            "25,3.8159",
-            "30,3.9949",
-            "35,4.1467",
-            "40,4.2785",
-            "45,4.3949",
-            "50,4.4992"
+            "0,1.0",
+            "1,1.5",
+            "2,1.75",
+            "3,1.875",
+            "4,1.9375",
+            "5,1.96875"
     })
     void calculateSumOfSeries(double number, double expectedSum) {
         double sum = seriesSumFinder.calculateSumOfSeries(number);
         Assertions.assertEquals(
                 expectedSum,
                 sum,
-                0.1,
+                0.01,
                 "The sum of the series is not correct!"
         );
     }
